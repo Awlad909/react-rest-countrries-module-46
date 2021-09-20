aa183913f06d582fdaedbd7ec40483329d2a1d43
Awlad909 <you@example.com>
2021-09-20 17:24:36 +0600

    added

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Countries></Countries>
+      
+    </div>
+  );
+}
+
+const Countries = (props) =>{
+    const [countries, setCountries] = useState([]);
+
+    useEffect( () =>{
+      fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+    },);
+  return(
+    <div>
+      <h1>Hello yo yo react and Countries mama!!</h1>
+      <h2>This is all Countries length:{countries.length}</h2>
+      <h3>Countries Name:{props.name}</h3>
+    </div>
+  );
+}
+
+export default App;
